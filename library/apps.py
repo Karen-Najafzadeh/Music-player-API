2f4d16a4adc66885c3e350701acaed0d810c909b
Karen <najafzadehkaren@gmail.com>
2023-09-12 20:48:35 +0330

    fixed the signal
    i forgot to add the signal to the app.py. so added it and also added the media to .gitignore and ran few manual tests to make sure everything's ok

--- library/apps.py
+++ library/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class LibraryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'library'
+    def ready(self) -> None:
+        import library.signals
