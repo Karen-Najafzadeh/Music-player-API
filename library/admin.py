c0bbaef5572971b58097188f96aea27c23fbd1bd
Karen <najafzadehkaren@gmail.com>
2023-09-12 19:31:08 +0330

    created the models and the admin of the library app

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user','first_name','last_name']
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ['title', 'artist']
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+@admin.register(Music)
+class MusicAdmin(admin.ModelAdmin):
+    list_display = ['title','file','album']
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['profile','music']
+
+@admin.register(Playlist)
+class PlaylistAdmin(admin.ModelAdmin):
+    list_display = ['name', 'profile']
+
+@admin.register(LikesPlaylist)
+class LikesPlaylistAdmin(admin.ModelAdmin):
+    list_display = ['profile']
--- library/models.py
+++ library/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    def first_name(self) -> str:
+        return self.user.first_name
+    def last_name(self) -> str:
+        return self.user.last_name
+    def __str__(self) -> str:
+        return self.user.username
+
+class Artist(models.Model):
+    name = models.CharField(max_length=255)
+    bio = models.TextField()
+    image = models.ImageField(upload_to='library/images/artists')
+    def __str__(self) -> str:
+        return self.name
+
+class Album(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='albums')
+    image = models.ImageField(upload_to='library/images/album')
+    date_release = models.DateField()
+    def __str__(self) -> str:
+        return self.title
+
+class Genre(models.Model):
+    title = models.CharField(max_length=255)
+    def __str__(self) -> str:
+        return self.title
+
+class Music(models.Model):
+    file = models.FileField(upload_to='library/music')
+    title = models.CharField(max_length=255)
+    artist = models.ManyToManyField(Artist, related_name='musics')
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name='musics')
+    lyric = models.TextField()
+    genre = models.ManyToManyField(Genre, related_name='musics')
+    def __str__(self) -> str:
+        return self.title
+
+class Review(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='reviews')
+    discription = models.TextField()
+    music = models.ForeignKey(Music, on_delete=models.CASCADE, related_name = 'reviews')
+    def __str__(self) -> str:
+        return self.profile.user.username
+    
+class Playlist(models.Model):
+    name = models.CharField(max_length=255)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='playlists')
+    songs = models.ManyToManyField(Music)
+
+    def __str__(self) -> str:
+        return f"{self.name} for {self.profile.user.first_name}"
+
+class LikesPlaylist(models.Model):
+    profile = models.OneToOneField(Profile, unique=True, on_delete=models.CASCADE)
+    songs = models.ManyToManyField(Music)
+
+    def __str__(self) -> str:
+        return f"{self.profile.user.first_name}'s likes"
